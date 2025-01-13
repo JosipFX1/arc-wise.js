c2cbed21c0d3023157ea3d6f6ed7600601bc8344
JosipFX <josip9510@gmail.com>
2025-01-13 10:53:18 +0100

    Added $clientInfo, Testing $reminder

--- arc.aoi/arcfunctions.js
+++ arc.aoi/arcfunctions.js
@@ -9,12 +9,46 @@ class ArcFunctions {
     const client = this.args.client;
 
     client.functionManager.createFunction({
-        name: "$hello",
-        params: [ "message" ], 
+        name: "$clientInfo",
+        params: [], 
         type: "aoi.js", 
         code: `
-        $sendMessage[{message}]`
+        *Ping:* \`$pingms\`
+        *Uptime:* \`$uptime\`
+        *Database Ping:* \`$databasePing\`
+        *RAM Usage:* \`$ram / $maxRam\`
+        *CPU Usage:* \`$cpu%\`
+        *In \`$guildCount\` Servers*`
     });
+
+    client.functionManager.createFunction({
+      name: "reminder",
+      params: ["message", "time", "userID", "channelID"],
+      type: "aoi.js",
+      code: `
+      $setTimeout[reminder;{time};{"message": "{message}", "user:" "{userID}", "channel": "{channelID}"}]
+      
+      $onlyIf[$userExists[{userID}]==true;❌ **User that you have Specified does not Exist!**]
+      $onlyIf[$channelExists[{channelID}]==true;❌ **Channel that you have Specified does not Exist!**]
+      $onlyIf[$charCount[{time}]<=3;❌ **Invalid Usage for \`time\` Parameter. Input normal Time ex. \`24h, 10m, 30s, 1D\`]
+      $onlyIf[{time}!=;❌ **Invalid Usage, Missing \`time\` Parameter!**]
+      $onlyIf[{channelID}!=;❌ **Invalid Usage, Missing \`channelID\` Parameter!**]
+      $onlyIf[{userID}!=;❌ **Invalid Usage, Missing \`userID\` Parameter!**]
+      $onlyIf[{message}!=;❌ **Invalid Usage, Missing \`message\` Parameter!**]`
+    })
+
+    client.timeoutCommand({
+      name: "reminder",
+      code: `
+      $sendDM[
+      **Hello <@$timeoutData[user]>! Don't forget about:**
+
+      $timeouData[message];$timeoutData[user]]
+      $channelSendMessage[$timeoutData[channel];
+      **Hello <@$timeoutData[user]>! Don't forget about:**
+      
+      $timeoutData[message]]`
+    })
     }
 }
 
