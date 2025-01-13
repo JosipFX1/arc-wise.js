ff9e67fd3f9472adbea8e49f9981c9a70513398c
JosipFX <josip9510@gmail.com>
2025-01-13 21:24:19 +0100

    fixing

--- arc.aoi/arcfunctions.js
+++ arc.aoi/arcfunctions.js
@@ -28,7 +28,7 @@ class ArcFunctions {
       code: `
       $setTimeout[reminder;$get[time];{"message": "$get[message]", "user": "$get[user]", "channel": "$get[channel]", "dmUser": "$get[dmUser]", "timeoutMessage": "$get[timeoutMessage]"}]
       
-      $onlyIf[$get[timeoutMessage]==true || $get[timeoutMessage]==false;❌ **Invalid Usage, Parameter "dmUser" must be either \`true\` or \`false\`!**]
+      $onlyIf[$get[timeoutMessage]==true||$get[timeoutMessage]==false;❌ **Invalid Usage, Parameter "dmUser" must be either \`true\` or \`false\`!**]
       $onlyIf[$userExists[$get[user]]==true;❌ **User that you have Specified does not Exist!**]
       $onlyIf[$channelExists[$get[channel]]==true;❌ **Channel that you have Specified does not Exist!**]
       $onlyIf[$get[time]!=;❌ **Invalid Usage, Missing \`time\` Parameter!**]
--- arc.aoi/index.js
+++ arc.aoi/index.js
@@ -6,16 +6,20 @@ module.exports = {
   ArcFunctions
 };
 
+setTimeout(async () => {
 console.log(chalk.red(`
 ╭──────────────────────────────────────────────────────────────────────────────────────────────╮
 │            arc.aoi                                                                           │
 │      Modified Version of aoi.js is installed.                                                │
 │ Please do not install official and use modified one because its meant to be used for arc.aoi │
 ╰──────────────────────────────────────────────────────────────────────────────────────────────╯`))
+}, 5000)
 
+setTimeout(async () => {
 console.log(chalk.blue(`
 ╭───────────────────────────────╮
 │            arc.aoi            │
 │         Version: ${version}        │
 │ https://discord.gg/MmJ3PQWVSS │
 ╰───────────────────────────────╯`))
+}, 5000)
